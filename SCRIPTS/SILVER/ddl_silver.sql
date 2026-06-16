1e760934e5aec3dca6067043e0cff6eec00b72a8
Daniel Alfred <115459705+danielalfred263@users.noreply.github.com>
2026-06-16 10:49:58 +0100

    Align column formatting in ddl_silver.sql

--- SCRIPTS/SILVER/ddl_silver.sql
+++ SCRIPTS/SILVER/ddl_silver.sql
@@ -12,14 +12,14 @@ DDL Script: Create Silver Tables
 IF OBJECT_ID ('silver.crm_cust_info','U') IS NOT NULL
 DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
-	cst_id			           INT,
+	cst_id			          INT,
 	cst_key			          NVARCHAR(50),
-	cst_firstname	      	NVARCHAR(50),
+	cst_firstname	      	  NVARCHAR(50),
 	cst_lastname		      NVARCHAR(50),
-	cst_material_status 	NVARCHAR(50),
+	cst_material_status 	  NVARCHAR(50),
 	cst_gndr		          NVARCHAR(50),
-	cst_create_date		    DATE,
-  dwh_create_date       DATETIME2 DEFAULT GETDATE()
+	cst_create_date			  DATE,
+  dwh_create_date             DATETIME2 DEFAULT GETDATE()
 );
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
 DROP TABLE silver.crm_prd_info;
@@ -30,8 +30,8 @@ CREATE TABLE silver.crm_prd_info(
 	prd_cost		    INT,
 	prd_line		    NVARCHAR(50),
 	prd_start_dt		DATETIME,
-	prd_end_dt		  DATETIME,
-  dwh_create_date DATETIME2 DEFAULT GETDATE()
+	prd_end_dt		    DATETIME,
+  dwh_create_date       DATETIME2 DEFAULT GETDATE()
 	);
 IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
 DROP TABLE silver.crm_sales_details;
@@ -43,24 +43,24 @@ CREATE TABLE silver.crm_sales_details(
 	sls_ship_dt		  DATE,
 	sls_due_dt		  DATE,
 	sls_quantity	  INT,
-	sls_price		    INT,
-	sls_sales		    INT,
-  dwh_create_date DATETIME2 DEFAULT GETDATE()
+	sls_price		  INT,
+	sls_sales		  INT,
+  dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 IF OBJECT_ID ('silver.erp_cust_az12','U') IS NOT NULL
 DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
 	cid		            NVARCHAR(50),
-	bdate		          DATE,
+	bdate		        DATE,
 	gen		            NVARCHAR(50),
-  dwh_create_date   DATETIME2 DEFAULT GETDATE()
+  dwh_create_date       DATETIME2 DEFAULT GETDATE()
 );
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
 DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101(
 	cid		          NVARCHAR(50),
-	cntry		        NVARCHAR(50),
-  dwh_create_date DATETIME2 DEFAULT GETDATE()
+	cntry		      NVARCHAR(50),
+  dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2','U') IS NOT NULL
@@ -70,6 +70,6 @@ CREATE TABLE bronze.erp_px_cat_g1v2(
 	cat		          NVARCHAR(50),
 	subcat		      NVARCHAR(50),
 	maintenance 	  NVARCHAR(50),
-  dwh_create_date DATETIME2 DEFAULT GETDATE()
+  dwh_create_date     DATETIME2 DEFAULT GETDATE()
 
 )
